95c8021eeca83cea349ca5044c342ccdcfe8fe4d
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-08-27 18:21:02 +0200

    Use assertion over expectation
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- src/Configuration/ProjectConfigurationFactory.php
+++ src/Configuration/ProjectConfigurationFactory.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Mezzio\Cors\Configuration;
 
 use Psr\Container\ContainerInterface;
+use Webmozart\Assert\Assert;
 
 final class ProjectConfigurationFactory
 {
     public function __invoke(ContainerInterface $container): ProjectConfiguration
     {
-        /** @var array<string,mixed> $parameters */
         $parameters = $container->get('config')[ProjectConfiguration::CONFIGURATION_IDENTIFIER] ?? [];
+        Assert::isMap($parameters);
         return new ProjectConfiguration($parameters);
     }
 }
