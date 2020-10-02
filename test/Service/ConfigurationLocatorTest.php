0fce61085554b9bae62ebb8e7dc5cdaf005b3da9
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-10-02 23:11:25 +0200

    qa: codestyle fixes
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/Service/ConfigurationLocatorTest.php
+++ test/Service/ConfigurationLocatorTest.php
@@ -357,9 +357,11 @@ final class ConfigurationLocatorTest extends TestCase
             ->method('isFailure')
             ->willReturn(false);
 
-        $routeConfigurationParameters = [RouteConfigurationInterface::PARAMETER_IDENTIFIER => [
-            'explicit' => true,
-        ]];
+        $routeConfigurationParameters = [
+            RouteConfigurationInterface::PARAMETER_IDENTIFIER => [
+                'explicit' => true,
+            ],
+        ];
 
         $matchingExplicitRouteResult
             ->expects($this->once())
